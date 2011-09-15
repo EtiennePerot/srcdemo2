771bd8c2df3765f183a0c50e15cff16e35c328bc
Etienne Perot <etienne@perot.me>
2011-09-15 22:17:59 +0000

    UI is getting there

--- src/net/srcdemo/ui/SrcDemoUI.java
+++ src/net/srcdemo/ui/SrcDemoUI.java
@@ -1,13 +1,133 @@
 package net.srcdemo.ui;
 
-import com.trolltech.qt.gui.QApplication;
-import com.trolltech.qt.gui.QMainWindow;
+import java.io.File;
 
-public class SrcDemoUI extends QMainWindow
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QFileDialog;
+import com.trolltech.qt.gui.QHBoxLayout;
+import com.trolltech.qt.gui.QLabel;
+import com.trolltech.qt.gui.QLineEdit;
+import com.trolltech.qt.gui.QPushButton;
+import com.trolltech.qt.gui.QSpinBox;
+import com.trolltech.qt.gui.QVBoxLayout;
+import com.trolltech.qt.gui.QWidget;
+
+public class SrcDemoUI extends QWidget
 {
-	public void main(final String[] args)
+	public static void main(final String[] args)
 	{
 		QApplication.initialize(args);
+		new SrcDemoUI();
 		QApplication.exec();
+	}
+
+	private QLineEdit backingDirectory;
+	private QPushButton backingDirectoryBrowse;
+	private QSpinBox blendRate;
+	private QLabel effectiveRecordingFps;
+	private QLabel effectiveRecordingFpsCommand;
+	private QLineEdit mountpoint;
+	private QPushButton mountpointBrowse;
+	private QSpinBox targetFps;
+
+	SrcDemoUI()
+	{
+		setWindowTitle(Strings.windowTitle);
+		initUI();
+		show();
+	}
+
+	private void initUI()
+	{
+		final QVBoxLayout vbox = new QVBoxLayout();
+		{
+			final QLabel label = new QLabel(Strings.step1);
+			vbox.addWidget(label);
+			final QHBoxLayout hbox = new QHBoxLayout();
+			mountpoint = new QLineEdit();
+			hbox.addWidget(mountpoint);
+			mountpointBrowse = new QPushButton(Strings.btnBrowse);
+			mountpointBrowse.clicked.connect(this, "onBrowseMountpoint()");
+			hbox.addWidget(mountpointBrowse);
+			vbox.addLayout(hbox);
+		}
+		{
+			final QLabel label = new QLabel(Strings.step2);
+			vbox.addWidget(label);
+			final QHBoxLayout hbox = new QHBoxLayout();
+			backingDirectory = new QLineEdit();
+			hbox.addWidget(backingDirectory);
+			backingDirectoryBrowse = new QPushButton(Strings.btnBrowse);
+			backingDirectoryBrowse.clicked.connect(this, "onBrowseBackingDirectory()");
+			hbox.addWidget(backingDirectoryBrowse);
+			vbox.addLayout(hbox);
+		}
+		{
+			{
+				final QLabel label = new QLabel(Strings.step3);
+				vbox.addWidget(label);
+				final QHBoxLayout hbox = new QHBoxLayout();
+				hbox.addWidget(new QLabel(Strings.lblTargetFps));
+				targetFps = new QSpinBox();
+				targetFps.setRange(12, 300);
+				targetFps.setSuffix(Strings.spnTargetFps);
+				targetFps.setValue(30);
+				targetFps.valueChanged.connect(this, "updateEffectiveRecordingFps()");
+				hbox.addWidget(targetFps);
+				vbox.addLayout(hbox);
+			}
+			{
+				final QHBoxLayout hbox = new QHBoxLayout();
+				hbox.addWidget(new QLabel(Strings.lblBlendRate));
+				blendRate = new QSpinBox();
+				blendRate.setRange(1, 99);
+				blendRate.setSuffix(Strings.spnBlendRate);
+				blendRate.setValue(25);
+				blendRate.valueChanged.connect(this, "updateEffectiveRecordingFps()");
+				hbox.addWidget(blendRate);
+				vbox.addLayout(hbox);
+			}
+			{
+				final QHBoxLayout hbox = new QHBoxLayout();
+				hbox.addWidget(new QLabel(Strings.lblEffectiveFps));
+				effectiveRecordingFps = new QLabel();
+				hbox.addWidget(effectiveRecordingFps);
+				vbox.addLayout(hbox);
+				vbox.addWidget(new QLabel("Make sure to set host_framerate before you start rendering!"));
+				effectiveRecordingFpsCommand = new QLabel();
+				vbox.addWidget(effectiveRecordingFpsCommand);
+			}
+			updateEffectiveRecordingFps();
+		}
+		setLayout(vbox);
+	}
+
+	private void onBrowseBackingDirectory()
+	{
+		final String selectedFolder = QFileDialog.getExistingDirectory(this, Strings.step1Dialog);
+		if (selectedFolder.length() > 0) {
+			backingDirectory.setText(selectedFolder);
+		}
+		if (!(new File(selectedFolder).isDirectory())) {
+			backingDirectory.setText("");
+		}
+	}
+
+	private void onBrowseMountpoint()
+	{
+		final String selectedFolder = QFileDialog.getExistingDirectory(this, Strings.step2Dialog);
+		if (selectedFolder.length() > 0) {
+			mountpoint.setText(selectedFolder);
+		}
+		if (!(new File(selectedFolder).isDirectory())) {
+			mountpoint.setText("");
+		}
+	}
+
+	private void updateEffectiveRecordingFps()
+	{
+		final int effectiveFps = blendRate.value() * targetFps.value();
+		effectiveRecordingFps.setText("" + effectiveFps);
+		effectiveRecordingFpsCommand.setText("host_framerate " + effectiveFps);
 	}
 }
---
+++ src/net/srcdemo/ui/Strings.java
@@ -0,0 +1,18 @@
+package net.srcdemo.ui;
+
+
+public interface Strings
+{
+	final String btnBrowse = "Browse...";
+	final String lblBlendRate = "Motion blur blend rate: ";
+	final String lblEffectiveFps = "Effective recording FPS: ";
+	final String lblTargetFps = "Final video FPS: ";
+	final String spnBlendRate = " frames per frame";
+	final String spnTargetFps = " frames per second";
+	final String step1 = "1. Select the folder where the game will save frames.";
+	final String step1Dialog = "Select the folder where you want the final frames to be saved.";
+	final String step2 = "2. Select the folder where you want the final frames to be saved.\n(Must be a different directory!)";
+	final String step2Dialog = "Select the folder where the game will save frames.";
+	final String step3 = "3. Set parameters";
+	final String windowTitle = "SrcDemo2";
+}
