0d8ad5496e886f0b2803768359ae23ac685757e6
Etienne Perot <etienne@perot.me>
2012-01-02 23:30:18 +0000

    Set formatter settings

--- src/net/srcdemo/video/image/ImageSavingTaskFactory.java
+++ src/net/srcdemo/video/image/ImageSavingTaskFactory.java
@@ -1,6 +1,5 @@
 package net.srcdemo.video.image;
 
-
 public abstract class ImageSavingTaskFactory
 {
 	public abstract ImageSavingTask buildSavingTask(final int sequenceIndex, final int[] pixelData, final int width,
